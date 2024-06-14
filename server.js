7d95deb609d1b1e1eeed32b6a1ceec8b63742cab
하승우 <winwoo4946@gmail.com>
2024-06-14 10:32:33 +0900

    test

---
+++ server.js
@@ -0,0 +1,15 @@
+try {
+    const jsonServer = require('json-server');
+    const server = jsonServer.create();
+    const router = jsonServer.router('db.json');
+    const middlewares = jsonServer.defaults();
+
+    server.use(middlewares);
+    server.use(router);
+    server.listen(3000, () => {
+        console.log('JSON Server is running on port 3000');
+    });
+} catch (e) {
+    console.log(e);
+}
+
